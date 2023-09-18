5c0261e8fc9661fdb88906836ea51462d064dbdb
DarKDinDoN <maxime.chicault@gmail.com>
2023-09-19 00:34:14 +0200

    Fix open at startup feature

--- module/hooks/startup.js
+++ module/hooks/startup.js
@@ -8,7 +8,9 @@ import { CONSTANTS } from "../shared/constants";
 export const openAtStartup = () => {
   // When fvtt is ready
   Hooks.once("ready", () => {
+    const openAtStartup = game.settings.get(CONSTANTS.MODULE_NAME, SETTINGS.OPEN_AT_STARTUP);
     const playerVisibility = game.settings.get(CONSTANTS.MODULE_NAME, SETTINGS.PLAYER_VISIBILITY);
+    if (!openAtStartup) return;
     if (!playerVisibility && !game.user.isGM) return;
     if (!game.canvas.scene) return;
 
